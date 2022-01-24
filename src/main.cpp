0fa7c43eda9aa024a1c86bdfff4fd6c343d50744
Daniel Pust <pust.daniel@outlook.com>
2022-01-24 08:45:39 +0100

    update (#1)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
-void setup() {
+//GPIO4 - on board LED
+//UART0 <-> comm with pc
+//GPIO0 - LED red
+
+int delayTime = 25;
+
+void setup()
+{
   // put your setup code here, to run once:
+  pinMode(0, OUTPUT);
+  pinMode(4, OUTPUT);
+  Serial.begin(115200);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  digitalWrite(0, false);
+  digitalWrite(4, true);
+  delay(delayTime);
+  digitalWrite(0, true);
+  digitalWrite(4, false);
+  delay(delayTime * 100);
+  Serial.write('*');
 }
