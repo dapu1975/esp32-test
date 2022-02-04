698a283a3751abbd6ba0df5aaf172ca5d99f9851
Daniel Pust <pust.daniel@outlook.com>
2022-02-04 08:26:15 +0100

    oled working

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,46 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiWire.h"
+#include <SimpleCLI.h>
+#include "HX711.h"
 
-//GPIO4 - on board LED
-//UART0 <-> comm with pc
-//GPIO0 - LED red
+// console interface uart
+// $1 = request parameter $1
+// $1 = 100 set parameter $1 to 100
+// $$ = show all parameter
+// $T = tara set current weight to zero
+// $C = calibrate
+// $R = restart
+// $RST=RST = reset all to default
 
-int delayTime = 25;
+// OLED circuit wiring
+#define I2C_ADDRESS 0x3C // 0X3C+SA0 - 0x3C or 0x3D
+#define RST_PIN -1       // Define proper RST_PIN if required.
+SSD1306AsciiWire oled;
 
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 26;
+const int LOADCELL_SCK_PIN = 27;
+HX711 scale;
+
+//-----------------------------------------------------------------------------
 void setup()
 {
-  // put your setup code here, to run once:
-  pinMode(0, OUTPUT);
-  pinMode(4, OUTPUT);
+  // initialize console
   Serial.begin(115200);
+  // initialize display
+  Wire.begin();
+  Wire.setClock(400000L);
+  oled.begin(&Adafruit128x32, I2C_ADDRESS);
+  oled.setFont(Adafruit5x7);
 }
-
+//-----------------------------------------------------------------------------
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  digitalWrite(0, false);
-  digitalWrite(4, true);
-  delay(delayTime);
-  digitalWrite(0, true);
-  digitalWrite(4, false);
-  delay(delayTime * 100);
-  Serial.write('*');
+  oled.clear();
+  oled.set2X();
+  oled.println("HX711 V0.1");
+  delay(500);
 }
+//-----------------------------------------------------------------------------
